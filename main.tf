181423df30b5191b5fa295a3ba410d6da6490497
vvygovskiy <82603867+vvygovskiy@users.noreply.github.com>
2024-01-02 09:48:38 -0800

    Create security group 2

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow averything out"
 
-  vpc_id - data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
