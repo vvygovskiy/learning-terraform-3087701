a11bda60e8b454e0e1fddecd101d4eee2a1020c8
vvygovskiy <82603867+vvygovskiy@users.noreply.github.com>
2024-01-03 15:26:07 -0800

    autoscaling

--- main.tf
+++ main.tf
@@ -33,20 +33,24 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "blog"
+  min_size = 1
+  max_size = 2 
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  image_id           = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
+
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
