75f0cfa9e564e5f3a4762d677bd8c862c85e9566
vvygovskiy <82603867+vvygovskiy@users.noreply.github.com>
2024-01-03 14:55:31 -0800

    array for sgs

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
 
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_vpc.security_group_id
+  security_groups = [module.blog_vpc.security_group_id]
 
   target_groups = [
    {
@@ -89,7 +89,7 @@ module "alb" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  
+
   name = "blog"
 
   vpc_id = module.blog_vpc.vpc_id
